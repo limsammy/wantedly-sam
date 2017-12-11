d9eddf21e5b43d8351a71b2ee9f03ada05450e0d
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-11 10:01:21 -0700

    Generated user model and controller and added routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Logged in as #{@user.username}"
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    if !current_user.nil?
+      user = current_user
+    else
+      not_found
+    end
+  end
+
+  def edit
+    user = current_user
+  end
+
+  def update
+    user = current_user
+    if user.update(user_params)
+      redirect_to dashboard_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :full_name, :address)
+  end
+end
